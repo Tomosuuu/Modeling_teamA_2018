feaf747cb7c4871b3e2e320044d7881b9536134c
Tatsuya Iwanaga <e165740@ie.u-ryukyu.ac.jp>
2018-01-25 02:25:23 +0900

    second

--- alarm_teamA/alarm_teamA/AppDelegate.swift
+++ alarm_teamA/alarm_teamA/AppDelegate.swift
@@ -14,16 +14,16 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
     var window: UIWindow?
     var backgroundTaskID : UIBackgroundTaskIdentifier = 0
 
-
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey: Any]?) -> Bool {
         // Override point for customization after application launch.
         return true
     }
 
-    //バックグラウンド遷移移行直前に呼ばれる
     func applicationWillResignActive(_ application: UIApplication) {
-
-        self.backgroundTaskID = application.beginBackgroundTask() {
+        // Sent when the application is about to move from active to inactive state. This can occur for certain types of temporary interruptions (such as an incoming phone call or SMS message) or when the user quits the application and it begins the transition to the background state.
+        // Use this method to pause ongoing tasks, disable timers, and invalidate graphics rendering callbacks. Games should use this method to pause the game.
+    
+        self.backgroundTaskID = application.beginBackgroundTask(){
             [weak self] in
             application.endBackgroundTask((self?.backgroundTaskID)!)
             self?.backgroundTaskID = UIBackgroundTaskInvalid
@@ -39,8 +39,8 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
         // Called as part of the transition from the background to the active state; here you can undo many of the changes made on entering the background.
     }
 
-    //アプリがアクティブになるごとに呼ばれる
     func applicationDidBecomeActive(_ application: UIApplication) {
+        // Restart any tasks that were paused (or not yet started) while the application was inactive. If the application was previously in the background, optionally refresh the user interface.
         
         application.endBackgroundTask(self.backgroundTaskID)
     }
